dbaa4d85c64a66d27d729425fb9e1ea76b8b16be
Trina Chatterjee <trinac0731@gmail.com>
2020-02-15 11:33:57 -0800

    added geolocation to maps

--- my-app/my-app/src/App.js
+++ my-app/my-app/src/App.js
@@ -1,6 +1,8 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
 import './App.css';
-import { Map, GoogleApiWrapper } from 'google-maps-react';
+import { Map, GoogleApiWrapper} from 'google-maps-react';
+import { InfoWindow, Marker } from 'google-maps-react';
+import CurrentLocation from './Map';
 
 const mapStyles = {
   width: '100%',
@@ -8,22 +10,49 @@ const mapStyles = {
 };
 
 export class MapContainer extends Component {
+  state = {
+    showingInfoWindow: false,  //Hides or the shows the infoWindow
+    activeMarker: {},          //Shows the active marker upon click
+    selectedPlace: {}          //Shows the infoWindow to the selected place upon a marker
+  };
+
+  onMarkerClick = (props, marker, e) =>
+    this.setState({
+      selectedPlace: props,
+      activeMarker: marker,
+      showingInfoWindow: true
+    });
+
+  onClose = props => {
+    if (this.state.showingInfoWindow) {
+      this.setState({
+        showingInfoWindow: false,
+        activeMarker: null
+      });
+    }
+  };
+
   render() {
     return (
-      <Map
+      <CurrentLocation
+        centerAroundCurrentLocation
         google={this.props.google}
-        zoom={14}
-        style={mapStyles}
-        initialCenter={{
-         lat: -1.2884,
-         lng: 36.8233
-        }}
-      />
+      >
+        <Marker onClick={this.onMarkerClick} name={'Trina\'s Current Location'} />
+        <InfoWindow
+          marker={this.state.activeMarker}
+          visible={this.state.showingInfoWindow}
+          onClose={this.onClose}
+        >
+          <div>
+            <h4>{this.state.selectedPlace.name}</h4>
+          </div>
+        </InfoWindow>
+      </CurrentLocation>
     );
   }
 }
 
 export default GoogleApiWrapper({
-  apiKey: 'API_KEY'
+  apiKey: 'AIzaSyBGxejpcRtzOGdlo7nRvEx1Q0n5gpl7Kvk'
 })(MapContainer);
-
