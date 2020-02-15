a6ab8cfa8edb30b7e55742158a55cac98cba58c0
Aaron Lamoreaux <lamoreauxaj@gmail.com>
2020-02-15 15:11:53 -0600

    added maps to main app

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 import { BrowserRouter as Router, Switch, Route, Redirect } from 'react-router-dom'
 import Home from './Home.js'
 import Nav from './Nav.js'
+import MapPage from './MapPage.js'
 import './App.scss'
 
 class App extends Component {
@@ -13,6 +14,7 @@ class App extends Component {
                         <div className="content">
                             <Switch>
                                 <Route exact path="/" component={Home}/>
+                                <Route exact path="/map" component={MapPage}/>
                                 <Redirect to="/404"/>
                             </Switch>
                         </div>
