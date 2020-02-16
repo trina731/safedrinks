b4769b3d9438ab749690b31f44e43d9ec473545c
Trina Chatterjee <trinac0731@gmail.com>
2020-02-15 19:13:07 -0800

    edited groups page

--- src/components/Groups.js
+++ src/components/Groups.js
@@ -1,18 +1,71 @@
 import React, { Component } from 'react'
-import ReactDOM from 'react-dom';
-import { Button} from 'react-bootstrap';
 import './Groups.scss'
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    Link
+  } from "react-router-dom";
 
-class Groups extends Component{
+const min = 10000000;
+const max = 99999999;
+const rand = Math.floor(min + Math.random() * (max - min));
+
+class Groups extends Component{ 
     render(){
-        return(
-            <div>
-                <font size = "6">Welcome! Create or join a group. </font>
-            <Button className="create-button">Create Group</Button>
-            <Button className="join-button">Join Group</Button>
-            </div>
-        )
+    return (
+        <Router>
+          <div>
+            <nav>
+              <ul>
+                <li>
+                  <Link to="/">Current Session</Link>
+                </li>
+                <li>
+                  <Link to="/create">Create Group</Link>
+                </li>
+                <li>
+                  <Link to="/join">Join Group</Link>
+                </li>
+              </ul>
+            </nav>
+    
+            {/* A <Switch> looks through its children <Route>s and
+                renders the first one that matches the current URL. */}
+            <Switch>
+              <Route path="/create">
+                <Create />
+              </Route>
+              <Route path="/join">
+                <Join />
+              </Route>
+              <Route path="/">
+                <Home />
+              </Route>
+            </Switch>
+          </div>
+        </Router>
+      );
     }
-}
+}  
+    function Home() {
+      return <h4>Current Session</h4>;
+    }
+    
+    function Create() {
+    return(<div>
+        <h4> Your group ID is: {rand}</h4>
+    </div>);
+    }
 
-export default Groups
+    function Join() {
+        return(<div>
+            <h4>Join group: </h4>
+            <input
+            type="text"
+         />
+        </div>);
+      }
+    
+    
+export default Groups;
